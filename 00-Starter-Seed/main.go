aac15a04bb5910f6e46aa3f1b02278d6da78c1b6
Boris <oreqizer@users.noreply.github.com>
2016-12-24 20:59:57 +0100

    main: Fix if statement
    'secret' should not be "", however, it is considered an error if it is not empty.

--- 00-Starter-Seed/main.go
+++ 00-Starter-Seed/main.go
@@ -29,7 +29,7 @@ func StartServer() {
         jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
           ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
             secret := os.Getenv("AUTH0_CLIENT_SECRET")			
-            if secret != "" {
+            if secret == "" {
               return nil, errors.New("AUTH0_CLIENT_SECRET is not set")
             }
             return secret, nil
